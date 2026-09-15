f0c44ed21483933944e391e073afd208b0e3e426
shivamaurya01 <mauryshiva8053@gmail.com>
2026-09-15 23:26:47 +0530

    Fix the editor page and home page

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -7,6 +7,8 @@ import { useNavigate } from 'react-router-dom'
 function Dashboard() {
     const {userData}=useSelector(state => state.user)
     const navigate=useNavigate();
+
+    
     return (
     <div className='min-h-screen bg-[#050505] text-white'>
       <div className='sticky top-0 z-40 backdrop-blur-xl bg-black/50 border-b border-white/10'>
